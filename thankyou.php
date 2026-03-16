d53e12726da4f1fbcadc7267ee58b793d657bfa0
Marek Cvoliga <marek.cvoliga@student.ukf.sk>
2026-03-16 12:12:48 +0100

    Rozkúskovanie/oddenie header a footer

---
+++ thankyou.php
@@ -0,0 +1,22 @@
+<?php
+$pageTitle = 'Ďakujeme';
+$extraCss  = ['css/banner.css'];
+include 'header.php';
+?>
+
+    <main>
+      <section class="banner">
+        <div class="container text-white">
+          <h1>Ďakujeme</h1>
+        </div>
+      </section>
+      <section class="container">
+        <div class="row">
+          <div class="col-100 text-center">
+              <h2>Ďakujeme za vyplnenie formulára</h2>
+          </div>
+        </div>
+      </section>
+    </main>
+
+<?php include 'footer.php'; ?>
