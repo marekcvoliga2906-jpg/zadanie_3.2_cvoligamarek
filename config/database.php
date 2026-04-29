c1cf4c6f4b214741e70f845713942db89b844cbc
Marek Cvoliga <marek.cvoliga@student.ukf.sk>
2026-04-29 18:26:19 +0200

    database create

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'dbname' => 'zadanie_3_2',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+];
